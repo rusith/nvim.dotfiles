1b236076c4d4f33ca6614fbd45f0dc75ad442424
Rusith <rusith@mail.com>
2023-03-28 22:32:10 +0200

    initial commit

---
+++ lua/rusith/packer.lua
@@ -0,0 +1,17 @@
+vim.cmd [[packadd packer.nvim]]
+
+-- packges
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  vim.cmd('colorscheme rose-pine')
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+end)
