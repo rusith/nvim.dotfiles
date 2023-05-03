a92fd546b0e81c99bbbad8551e01245029d35fd6
Rusith <rusith@mail.com>
2023-05-03 16:35:42 +0200

    make the background transparent

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,8 @@
 function ColorMyPencils(color) 
 	color = color or "catppuccin"
   vim.cmd.colorschem(color)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
